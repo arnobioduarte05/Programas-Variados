e93051652e34196f518fad447b0b9688ad050a97
Arnóbio Duarte <arnobiojunior2010@gmail.com>
2016-06-09 08:06:48 -0300

    Fibonacci em java

---
+++ fibonacci.java
@@ -0,0 +1,19 @@
+PROGRAMA FIBONACCI EM JAVA
+import java.util.Scanner;
+
+public class java {
+	public static void main (String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("N: ");
+		int n = input.nextInt();
+		int a = 1, b = 0;
+
+		System.out.println(b + "\n" + a);
+
+		for (int i = 0; i <= n; i++) {
+			a += b;
+			b = a - b;
+			System.out.println(a);
+		}
+	}
+}

0d89dd2af48a13f644043bd25343b9efde209b1b
Arnóbio Duarte <arnobiojunior2010@gmail.com>
2016-06-09 08:15:14 -0300

    Fibonacci em PHP

---
+++ fibonacci.php
@@ -0,0 +1,13 @@
+//FIBONACCI EM PHP
+<?php
+  function fibonacci($count){
+    $a = 1;
+    $b = 0;
+    $sequence = $b . ", " . $a;
+    for($i = 0; $i < $count; $i++){
+      $a += $b;
+      $b = $a - $b;
+      $sequence .= ', ' . $a;
+    }
+    return $sequence;
+  }
